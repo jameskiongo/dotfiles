8b3b4f72aa20db8c1b60df03501a14f559244701
jameskiongo <kiongojames2000@gmail.com>
2025-05-12 22:35:28 +0300

    added obsidian

--- .config/nvim/lua/james/core/options.lua
+++ .config/nvim/lua/james/core/options.lua
@@ -2,6 +2,7 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
+opt.conceallevel = 2 -- conceal level for markdown
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
---
+++ .config/nvim/lua/james/plugins/obsidian.lua
@@ -0,0 +1,78 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   -- refer to `:h file-pattern` for more examples
+	--   "BufReadPre path/to/my-vault/*.md",
+	--   "BufNewFile path/to/my-vault/*.md",
+	-- },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp", -- For completion
+		"nvim-telescope/telescope.nvim", -- For search
+
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "personal",
+				path = "/Users/jameskiongo/Documents/coding/vault/personal",
+			},
+			{
+				name = "projects",
+				path = "/Users/jameskiongo/Documents/coding/vault/projects",
+			},
+		},
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+
+		notes_subdir = "notes",
+		daily_notes = {
+			-- Optional, if you keep daily notes in a separate directory.
+			folder = "notes/dailies",
+			-- Optional, if you want to change the date format for the ID of daily notes.
+			date_format = "%Y-%m-%d",
+			-- Optional, if you want to change the date format of the default alias of daily notes.
+			alias_format = "%B %-d, %Y",
+			-- Optional, default tags to add to each new daily note created.
+			default_tags = { "daily-notes" },
+			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+			template = nil,
+		},
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		new_notes_location = "notes_subdir",
+	},
+}
