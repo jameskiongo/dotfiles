d3b83efcb7e007236227a49fe4650fb79b220e94
jameskiongo <kiongojames2000@gmail.com>
2025-07-08 17:22:25 +0300

    added transparent plugin

--- .config/nvim/lua/james/core/options.lua
+++ .config/nvim/lua/james/core/options.lua
@@ -1,7 +1,9 @@
 local opt = vim.opt -- for conciseness
 
 -- line numbers
+opt.termguicolors = true
 opt.relativenumber = true -- show relative line numbers
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "markdown",
 	callback = function()
@@ -30,7 +32,6 @@ opt.cursorline = true -- highlight the current cursor line
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- .config/nvim/lua/james/plugins/bufferline.lua
+++ .config/nvim/lua/james/plugins/bufferline.lua
@@ -1,14 +1,18 @@
 return {
 	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		options = {
 			mode = "tabs",
+			themeable = true,
 			show_close_icon = false,
 			show_buffer_close_icons = false,
 			separator_style = "line",
 			text_align = "center",
+			indicator = {
+				style = "none",
+			},
 		},
 	},
 }
