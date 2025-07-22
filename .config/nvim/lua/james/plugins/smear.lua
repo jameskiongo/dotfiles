9a21d3d57eb2efe37acf4c04ef3aea4b47f4dee1
jameskiongo <kiongojames2000@gmail.com>
2025-07-22 11:15:36 +0300

    added tiny diagnostics plugin

---
+++ .config/nvim/lua/james/plugins/smear.lua
@@ -0,0 +1,16 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	-- enabled = false,
+	opts = {
+		enabled = true,
+		transparent_bg_fallback_color = "#ffffff",
+		smear_between_buffers = false,
+		stiffness = 0.8,
+		trailing_stiffness = 0.5,
+		distance_stop_animating = 0.5,
+		stiffness_insert_mode = 0.7,
+		trailing_stiffness_insert_mode = 0.7,
+		damping = 0.8,
+		damping_insert_mode = 0.8,
+	},
+}
